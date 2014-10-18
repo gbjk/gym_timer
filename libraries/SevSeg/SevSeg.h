e9b7f3e23404e25b6df43a956c45954aa95b4813
Gareth Kirwan <gbjk@thermeon.com>
2014-10-18 17:18:09 +0100

    Rewritten SevSeg for 4" constant direct current.

--- libraries/SevSeg/SevSeg.h
+++ libraries/SevSeg/SevSeg.h
@@ -1,15 +1,6 @@
-//Written by Dean Reading, 2012.  deanreading@hotmail.com
-//See .cpp file for info
-
 #ifndef SevSeg_h
 #define SevSeg_h
-
-#if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
-#else
-#include "WProgram.h"
-#endif
-
 
 class SevSeg
 {
@@ -18,8 +9,7 @@ public:
   SevSeg();
 
   //Public Functions
-  void Begin(boolean mode_in,byte C1, byte C2, byte C3, byte C4, byte UC1, byte UC2, byte UC3, byte UC4, byte UC5, byte UC6, byte UC7, byte UC8);
-
+  void begin();
   void NewNum(char display[4], byte decimal_place);
 
   void PrintOutput();
@@ -35,9 +25,7 @@ private:
 
   //Private Variables
   boolean mode, DigitOn, DigitOff, SegOn, SegOff;
-  byte DigitPins[4];
-  byte SegmentPins[8];
-  boolean lights[4][8];
+  byte digits[4];
   byte hideNum;
 };
 
