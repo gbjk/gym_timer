7000de06c2939c507c5cf6161c3eaf5d8904813b
Gareth Kirwan <gbjk@thermeon.com>
2015-02-15 15:44:28 +0000

    Rewrite to abstract timer phases.

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -10,3 +10,12 @@ Put the unit to sleep
 * **Mode**  
 Toggle the round configuration mode. If a timer was in progress, it's stopped.
 * 
+
+# Operation of timer
+
+Once the timer is started:
+* **go** displays for 1 second, with one beep
+* The timer starts showing your configured round time, and countdown begins
+* When the timer reaches zero, **rESt** appears for 3 seconds, and the timer beeps 3 times
+* The rest timer starts
+* The timer restarts from step 1.
--- timer.ino
+++ timer.ino
@@ -11,29 +11,18 @@
 #include <SevSeg.h>
 #include <SPI.h>
 
-#define ALARM_DURATION 4000
-#define WARN_DURATION  2000
-
 #define USE_BUZZER 1
-
-#define DEFAULT_MINS 0
-#define DEFAULT_SECS 8
-
-#define DEFAULT_REST_MINS 0
-#define DEFAULT_REST_SECS 5
 
 #define IR_PIN     14
 #define BUZZ_PIN   19
 
+// MODE constants
 #define OFF             1
 #define WAIT            2
-#define ALARM_COUNTDOWN 3
-#define ALARM           4
-#define EDIT            5
-#define EXPECT_PROGRAM  6
-#define REST            7
-#define REST_COUNTDOWN  8
-#define WARN            9
+#define EDIT            3
+#define TIMER           4
+
+#define EXPECT_PROGRAM  7
 
 #define BUTTON_0     0xFF6897
 #define BUTTON_1     0xFF30CF
@@ -58,31 +47,45 @@
 IRrecv irrecv(IR_PIN);
 decode_results results;
 
-int mode = 2;
-bool flash_state;
+int mode  = WAIT;
+
+struct timer_phase {
+  int             beep_duration;  // How long to beep for.
+  bool            show_time;      // Whether to show the time, or the display char
+  char            display[5];
+  int             mins;
+  int             secs;
+  unsigned long   end_time;
+  unsigned long   end_beep;
+};
+
+#define ALARM_PHASE 1
+#define REST_PHASE  3
+#define PHASES      4
+
+struct timer_phase timer_phases[PHASES] = {
+  { 1, 0, " g0 ", 0, 1 },
+  { 0, 1, "",     0, 8 },     // Default to 0.08 timer
+  { 3, 0, "rEst", 0, 3 },
+  { 0, 1, "" },               // Default to 0.05 rest
+  };
+
+int current_phase_index = ALARM_PHASE;
+timer_phase current_phase       = timer_phases[ current_phase_index ];
+
+// Contexts: Beeping during TIMER, or Flashing during editting
+bool toggle = 0;
 byte edit_digit;
 
+unsigned long time;
 unsigned long last_tick;
 unsigned long last_sec;
-unsigned long alarm_time;
-unsigned long warn_start;
-
-int expect = 0;
-
-int mins = DEFAULT_MINS;
-int secs = DEFAULT_SECS;
-int last_mins;
-int last_secs;
-
-int rest_mins = DEFAULT_REST_MINS;
-int rest_secs = DEFAULT_REST_SECS;
 
 const unsigned int powers [4] = {1000,100,10,1};
 
 SevSeg sevseg;
 
-void setup()
-{
+void setup(){
 
   // Buzzer
   pinMode(BUZZ_PIN, OUTPUT);
@@ -96,144 +99,124 @@ void setup()
   sevseg.begin();
 
   irrecv.enableIRIn(); 
-}
-
-void loop() {
-
-  bool show = loop_for_mode();
-
-  if (!show){
-    sevseg.NewNum("    ");
   }
+
+void loop(){
+
+  loop_for_mode();
 
   sevseg.PrintOutput();
 
   if (irrecv.decode(&results)) {
     if (results.value != REPEAT){
       handle_key(results.value);
-    }
+      }
     irrecv.resume(); // Receive the next value
-  }
+    }
 
   sevseg.ShowAll();
-}
+  }
 
-bool loop_for_mode(){
-  bool show = mode != OFF;
+// See DOCUMENTATION.md
+void loop_for_mode(){
 
-  unsigned long mils = tick_clock();
-
-  if (mode == WARN){
-    if (USE_BUZZER){
-      digitalWrite(BUZZ_PIN, HIGH);
+  if (mode == OFF || mode == WAIT){
+    return;
     }
-  }
 
-  if (mode == ALARM_COUNTDOWN || mode == REST_COUNTDOWN){
-    do_countdown(mils);
-  }
+  if (mode == TIMER){
+    do_timer();
+    }
   else if (mode == EDIT){
+/*
     if (!flash_state){
       // Flash digit we're editting
       sevseg.HideNum(edit_digit);
       }
+*/
     }
-  else if (mode == EXPECT_PROGRAM){
-    if (!flash_state){
-      show = false;
-    }
-  }
-
-  if (mode == ALARM){
-    sevseg.NewNum("0555");
-    if (flash_state){
-      if (USE_BUZZER){
-        digitalWrite(BUZZ_PIN, HIGH);
-      }
-    }
-    else {
-      digitalWrite(BUZZ_PIN, LOW);
-      show = false;
-    }
-  }
-
-  return show;
 }
 
-unsigned long tick_clock(){
-  unsigned long mils = millis();
+void do_timer(){
+  time = millis();
 
-  int tick_rate = mode == EDIT ? 400 : 1000;
+  if (time > current_phase.end_time){ 
+    switch_phase();
+    }
+  else {
+    if (time - last_sec >= 1000){
+      current_phase.secs -= 1;
+      if (current_phase.secs < 0){
+        current_phase.mins -= 1;
+        current_phase.secs = 59;
+        }
+      last_sec = time;
 
-  if (mils - last_tick > tick_rate){
-
-    if ((mode == REST || mode == WARN) && (mils - warn_start > WARN_DURATION)){
-      if (mode == REST){
-        mode = REST_COUNTDOWN;
-      }
-      else if (mode == WARN){
-        mode = ALARM_COUNTDOWN;
-        digitalWrite(BUZZ_PIN, LOW);
+      if (time > current_phase.end_beep){
+        if (toggle){
+          digitalWrite(BUZZ_PIN, LOW);
+          toggle = 0;
+          }
+        }
+      else {
+        digitalWrite(BUZZ_PIN, toggle ? HIGH : LOW);
+        toggle = toggle ? 0 : 1;
+        }
       }
     }
 
-    flash_state = flash_state ? 0 : 1;
-    last_tick = mils;
-
-    if (!flash_state){
-
-      // Only end an alarm as flash state goes down
-      if (mode == ALARM && mils - alarm_time > ALARM_DURATION){
-        end_alarm();
-      }
+  if (current_phase.show_time){
+    set_time();
+    }
+  else {
+    sevseg.NewNum(current_phase.display);
     }
   }
-  return mils;
-}
+
+void switch_phase(){ 
+  end_phase();
+
+  current_phase_index += 1;
+  if (current_phase_index >= PHASES){
+    current_phase_index = 0;
+    }
+  current_phase = timer_phases[ current_phase_index ];
+
+  start_phase();
+  }
+
+void start_phase(){
+  current_phase.end_time = time + ( ( (current_phase.mins * 60) + current_phase.secs ) * 1000 );
+  if (current_phase.beep_duration){
+    current_phase.end_beep = time + ( current_phase.beep_duration * 1000 );
+    }
+  toggle = 1;
+  }
+
+void end_phase(){
+  // Make sure we're not flash down, buzzering, etc
+  digitalWrite(BUZZ_PIN, LOW);
+  toggle = 1;
+  }
+
+void start_timer(){
+  mode = TIMER;
+
+  current_phase = timer_phases[ 0 ];
+  start_phase();
+  }
+
+void end_timer(){
+  mode = WAIT;
+  end_phase();
+  }
 
 void set_time(){
   char display_timer[4];
+
   // Switch back to allowing decimal minutes
-  if (mode == EDIT){
-    sprintf(display_timer, "%02i%02i", mins, secs);
-    }
-  else {
-    sprintf(display_timer, "%02i%02i", mins, secs);
-    }
+  sprintf(display_timer, "%02i%02i", current_phase.mins, current_phase.secs);
   sevseg.NewNum(display_timer , 2);
-}
-
-void do_countdown(unsigned long mils){
-  if (mils - last_sec >= 1000){
-    if (mins == 0 && secs == 0){
-      if (mode == ALARM_COUNTDOWN){
-        mode = ALARM;
-
-        alarm_time = mils;
-
-        // Ensure we're flashing up
-        flash_state = 1;
-      }
-      else if (mode == REST_COUNTDOWN){
-        mins = last_mins;
-        secs = last_secs;
-        start_alarm_countdown();
-      }
-
-      last_tick = last_sec = millis();
-
-      return;
-    }
-    else {
-      secs -= 1;
-      if (secs < 0){
-        mins -= 1;
-        secs = 59;
-      }
-      last_sec = mils;
-    }
-  set_time();
-  }
 }
 
 void handle_key(unsigned long code){
@@ -277,7 +260,7 @@ void handle_key(unsigned long code){
 }
 
 void handle_mute(){
-  end_alarm();
+  end_timer();
   }
 
 void handle_swap(){
@@ -285,71 +268,51 @@ void handle_swap(){
   }
 
 void handle_play(){
-  if (mode == ALARM){
-    end_alarm();
+  if (mode == TIMER){
+    end_timer();
     }
   else {
-    start_alarm_countdown();
+    start_timer();
     }
   }
 
 void handle_power(){
-  // TODO - Handle any current modes
   mode = mode == OFF ? WAIT : OFF;
+  if (mode == WAIT){
+    enter_wait();
+    }
+  }
+
+void enter_wait(){
   set_time();
+  toggle = 1;
+  digitalWrite(BUZZ_PIN, LOW);
+  mode = WAIT;
   }
 
 void handle_mode(){
-  if (mode == ALARM){
-    end_alarm();
-  }
+  if (mode == TIMER){
+    end_timer();
+    enter_wait();
+    }
   else {
-    int old_mode = mode;
     mode = mode == EDIT ? WAIT : EDIT;
     if (mode == EDIT){
-      if (old_mode == ALARM_COUNTDOWN || old_mode == ALARM || old_mode == REST || old_mode == REST_COUNTDOWN){
-        secs = last_secs;
-        mins = last_mins;
-      }
+      current_phase = timer_phases[ ALARM_PHASE ];
 
       // Need to run set_time anyway, to make sure we switch to a leading 0
       set_time();
 
       edit_digit = 0;
+      }
     }
   }
-}
 
 void handle_number(int new_number){
   if (mode == EDIT){
-    if (edit_digit == 0){
-      if (new_number > 6){
-        new_number = 6;
-        }
-      int decimal = mins % 10;
-      mins = (10 * new_number) + decimal;
+    edit_number(new_number);
     }
-    else if (edit_digit == 1){
-      int decimal = mins / 10;
-      mins = (10 * decimal ) + new_number;
-    }
-    else if (edit_digit == 2){
-      if (new_number > 6 && edit_digit % 2 == 0){
-        new_number = 6;
-        }
-      int decimal = secs % 10;
-      secs = (10 * new_number) + decimal;
-    }
-    else {
-      int decimal = secs / 10;
-      secs = (10 * decimal) + new_number;
-    }
-    edit_digit += 1;
-    if (edit_digit > 3){
-      edit_digit = 0;
-    }
-  set_time();
-  }
+/*
   else if (mode == EXPECT_PROGRAM){
     char display_pr[4];
     sprintf(display_pr, "Pr %1i", new_number);
@@ -357,34 +320,37 @@ void handle_number(int new_number){
     sevseg.NewNum(display_pr, 5);
     mode = WAIT;
   }
-}
-
-void start_alarm_countdown(){
-  mode = mode == ALARM_COUNTDOWN ? WAIT : WARN;
-
-  last_secs = secs;
-  last_mins = mins;
-
-  last_tick = last_sec = millis();
-
-  sevseg.NewNum(" go ");
-
-  // Make sure we're starting up, for the buzzer
-  flash_state = 1;
+*/
   }
 
-void end_alarm(){
-  digitalWrite(BUZZ_PIN, LOW);
+void edit_number(int new_number){
+  // Nothing greater than 6 in decimal minutes or seconds
+  if (new_number > 6 && edit_digit % 2 == 0){
+    new_number = 6;
+    }
 
-  // Reset the default display
-  mode = REST;
-  warn_start = millis();
+  if (edit_digit == 0){
+    int remainder = current_phase.mins % 10;
+    current_phase.mins = (10 * new_number) + remainder;
+    }
+  else if (edit_digit == 1){
+    int decimal = current_phase.mins / 10;
+    current_phase.mins = (10 * decimal ) + new_number;
+    }
+  else if (edit_digit == 2){
+    int remainder = current_phase.secs % 10;
+    current_phase.secs = (10 * new_number) + remainder;
+    }
+  else {
+    int decimal = current_phase.secs / 10;
+    current_phase.secs = (10 * decimal) + new_number;
+    }
 
-  // TODO, real values
-  mins = DEFAULT_REST_MINS;
-  secs = DEFAULT_REST_SECS;
+  edit_digit += 1;
 
-  last_tick = last_sec = millis();
+  if (edit_digit > 3){
+    edit_digit = 0;
+    }
 
-  sevseg.NewNum("rE5t");
+  set_time();
   }
